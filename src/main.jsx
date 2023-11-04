3bd112644f63a132ac84a094572852c45d4bc818
Syed Ashiq <imsyedashiq@gmail.com>
2023-11-04 23:23:24 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home';
+import Root from './pages/Root';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return(
+        <div>
+             <p> HELLO I Am Login </p>
+        </div>
+    )}
+export default Login;
